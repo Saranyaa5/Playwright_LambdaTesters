3b851f4e723303d8504f29d1efb7749a7569d532
Saranyaa5 <2k21cse128@kiot.ac.in>
2025-07-10 10:07:12 +0530

    created pom project structure

--- src/hooks/pageFixture.ts
+++ src/hooks/pageFixture.ts
@@ -4,6 +4,6 @@ import { Logger } from 'winston';
 
 export const pageFixture = {
     page: undefined as Page | undefined,  // Remove @ts-ignore and make it optional
-    logger: undefined as Logger | undefined,
+    logger: undefined as Logger | undefined
    
 }
